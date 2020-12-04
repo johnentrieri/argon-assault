549cbe346f97fe9618833f5767d7955baf9bb371
John Entrieri <john.entrieri@gmail.com>
2020-12-04 13:47:35 -0500

    Added Splash Screen

---
+++ Assets/Scripts/SplashLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashLoad : MonoBehaviour
+{
+    [SerializeField] TextMesh loadText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        loadText.text = "Loading...";
+        Invoke("LoadLevelOne", 2);
+    }
+
+    private void LoadLevelOne() {
+        SceneManager.LoadScene(1);
+    }
+}
