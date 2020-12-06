80b92f4eaaa3196628f044880a2805c37cf7194b
John Entrieri <john.entrieri@gmail.com>
2020-12-06 12:51:59 -0500

    Added Scoreboard

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    private Text scoreText;
+    private int score = 0;    
+    [SerializeField] int scorePerHit = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit() {
+        score += scorePerHit;
+        scoreText.text = score.ToString();
+    }
+}
